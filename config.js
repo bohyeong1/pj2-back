bf3c3dde927474f71c2666458e316a3968974059
서보형 <tjqhgud9104@naver.com>
2024-05-29 00:05:12 +0900

    2024-5-29 backend

--- config.js
+++ config.js
@@ -7,4 +7,8 @@ dotenv.config() // process.env 객체에 환경변수 설정
 module.exports = {
   MONGODB_URL: process.env.MONGODB_URL,
   JWT_SECRET: process.env.JWT_SECRET,
-}
+  BUCKET_NAME: process.env.BUCKET_NAME,
+  ACCESS_KEY: process.env.ACCESS_KEY,
+  SECRET_KEY: process.env.SECRET_KEY,
+  ENDPOINT: process.env.ENDPOINT
+  }
--- src/routes/registerRt.js
+++ src/routes/registerRt.js
@@ -2,6 +2,16 @@ const express = require('express')
 const Accomodation = require('../models/Accomodation')
 const expressAsyncHandler = require('express-async-handler')
 const router = express.Router()
+const multer = require('multer')
+const AWS = require('aws-sdk')
+const {v4 : uuidv4} = require('uuid')
+const config = require('../../config')
+
+
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage })
+
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////라우터
@@ -34,6 +44,79 @@ router.post('/register',expressAsyncHandler(async(req,res,next)=>{
         res.status(401).json('에러발생')
     }
 }))
+
+
+///////////////////////이미지 한개 날리기
+router.post('/img',upload.single('homeImage') ,expressAsyncHandler(async(req,res,next)=>{
+
+    console.log(req.file)
+
+    const S3 = new AWS.S3({
+        endpoint: new AWS.Endpoint(config.ENDPOINT),
+        region: 'kr-standard',
+        credentials: {
+          accessKeyId: config.ACCESS_KEY,
+          secretAccessKey: config.SECRET_KEY,
+        },
+      });    
+      
+
+    
+      const imageName = uuidv4(); // 랜덤 이미지 이름 생성
+      await S3.putObject({
+        Bucket: config.BUCKET_NAME,
+        Key: `${imageName}.PNG`,
+        ACL: 'public-read',
+        // ACL을 지우면 전체공개가 되지 않습니다.
+        Body: req.file.buffer,
+        ContentType: 'image/png', // 파일 타입을 png로 지정
+      }).promise();
+    
+      res.json({
+        imageLink: `${config.ENDPOINT}/${config.BUCKET_NAME}/${imageName}.PNG`,
+      });
+
+}))
+
+
+///////////////////////이미지 여러개 날리기/ 초기 등록 5개로 제한
+router.post('/imgs',upload.array('homeImage',5) ,expressAsyncHandler(async(req,res,next)=>{
+
+  console.log(req.files)
+
+
+  const S3 = new AWS.S3({
+      endpoint: new AWS.Endpoint(config.ENDPOINT),
+      region: 'kr-standard',
+      credentials: {
+        accessKeyId: config.ACCESS_KEY,
+        secretAccessKey: config.SECRET_KEY,
+      },
+    });     
+    
+    const imageLinks = []            //////////db에 저장하는 이미지 링크 string값
+
+    ///////////////////이미지 오브젝트 스토리지로 전송 /////하나하나 전송해야 하는지? 동시에 전송하는 방법없는지????
+    for(let i = 0; i<req.files.length; i++){
+      const imageName = uuidv4(); // 랜덤 이미지 이름 생성
+
+      await S3.putObject({
+        Bucket: config.BUCKET_NAME,
+        Key: `${imageName}.PNG`,
+        ACL: 'public-read',
+        // ACL을 지우면 전체공개가 되지 않습니다.
+        Body: req.files[i].buffer,
+        ContentType: 'image/png', // 파일 타입을 png로 지정
+      }).promise();   
+
+      imageLinks.push(`${config.ENDPOINT}/${config.BUCKET_NAME}/${imageName}.PNG`)
+    }
+    
+    res.json({
+      imageLink: imageLinks,
+    });
+}))
+
 
 
 
